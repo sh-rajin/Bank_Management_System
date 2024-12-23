00a0326b1258d87f5974dcee44902730509da2d0
sh_rajin <shrajin7@gmail.com>
2024-12-24 00:33:27 +0600

    database added

--- bank_management/settings.py
+++ bank_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -92,16 +93,23 @@ WSGI_APPLICATION = 'bank_management.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+    'default': dj_database_url.config(     
+            default='postgresql://mamarbank_ihx4_user:J9OOCDZ9n2QtWdvgTbA1jRXElc11gJqS@dpg-ctkqotjqf0us73849ti0-a.oregon-postgres.render.com/mamarbank_ihx4',  
+            )
     }
-}
 
 
 # Password validation
